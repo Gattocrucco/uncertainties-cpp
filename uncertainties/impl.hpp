82ea7264a49a1efada28d7a100fdbde8a57f3dbb
Giacomo Petrillo <info@giacomopetrillo.com>
2019-05-13 11:54:20 +0200

    copyright notices

--- uncertainties/impl.hpp
+++ uncertainties/impl.hpp
@@ -1,3 +1,22 @@
+// impl.hpp
+//
+// Copyright (c) Giacomo Petrillo 2019
+//
+// This file is part of uncertainties-cpp.
+//
+// uncertainties-cpp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// uncertainties-cpp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with uncertainties-cpp.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef UNCERTAINTIES_IMPL_HPP_8AC76B25
 #define UNCERTAINTIES_IMPL_HPP_8AC76B25
 
--- uncertainties/io.hpp
+++ uncertainties/io.hpp
@@ -1,3 +1,22 @@
+// io.hpp
+//
+// Copyright (c) Giacomo Petrillo 2019
+//
+// This file is part of uncertainties-cpp.
+//
+// uncertainties-cpp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// uncertainties-cpp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with uncertainties-cpp.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef UNCERTAINTIES_IO_HPP_6DDCDE20
 #define UNCERTAINTIES_IO_HPP_6DDCDE20
 

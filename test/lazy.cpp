04b2179e03389d33fb6775854f95120d24efe1c9
Giacomo Petrillo <info@giacomopetrillo.com>
2019-05-21 23:02:57 +0200

    check LAZYPROP thread-safety

--- test/lazy.cpp
+++ test/lazy.cpp
@@ -1,16 +1,29 @@
 #include <cassert>
+#include <thread>
 
 #include <uncertainties/core.hpp>
 
 namespace unc = uncertainties;
 
+void check() {
+    const bool start = unc::lazyprop();
+    for (int i = 0; i < 10000; ++i) {
+        UNCERTAINTIES_LAZYPROP(true) {
+            assert(unc::lazyprop());
+            UNCERTAINTIES_LAZYPROP(false) {
+                assert(not unc::lazyprop());
+            }
+            assert(unc::lazyprop());
+        }
+        assert(start == unc::lazyprop());
+    }
+}
+
 int main() {
     assert(not unc::lazyprop());
-    UNCERTAINTIES_LAZYPROP(true) {
-        assert(unc::lazyprop());
-        UNCERTAINTIES_LAZYPROP(false) {
-            assert(not unc::lazyprop());
-        }
-    }
-    assert(not unc::lazyprop());
+    check();
+    std::thread t1(check), t2(check);
+    t1.join();
+    t2.join();
+    check();
 }

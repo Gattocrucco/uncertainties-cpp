027bcc197f8351e8657a6deb58c107bb84b456d4
Giacomo Petrillo <info@giacomopetrillo.com>
2019-06-19 13:15:57 +0200

    improvements to fit-plot.py

--- dev/fit-plot.py
+++ dev/fit-plot.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import uncertainties
 import numpy as np
 
-SIGMA_FACTOR = 5
+SIGMA_FACTOR = 4
 
 table = np.load('fit.npy', mmap_mode='r')
 N = len(table[0]['estimate'])
@@ -19,36 +19,97 @@ def ms(s):
 
 estimates = table['estimate'].T
 biases = table['bias'].T
+altbiases = table['alt_bias'].T
 covs = table['cov'].T
 sigmas = np.sqrt(np.einsum('iim->im', covs))
+
+ok = True
+for i in range(N):
+    ok &= np.abs(biases[i]) < SIGMA_FACTOR * sigmas[i]
+
+altok = True
+for i in range(N):
+    altok &= np.abs(altbiases[i]) < SIGMA_FACTOR * sigmas[i]
 
 for i in range(N):
     ax = axs[i][i]
     
     noncorr = estimates[i]
-    corr = estimates[i] - np.where(np.abs(biases[i]) < SIGMA_FACTOR * sigmas[i], biases[i], 0)
+    corr = estimates[i] - np.where(ok, biases[i], 0)
+    altcorr = estimates[i] - np.where(altok, altbiases[i], 0)
     ax.hist(
-        noncorr, bins='auto', histtype='step',
-        label='not corrected\nbias = {}'.format(ms(noncorr - true_par[i]))
+        noncorr, bins='auto', histtype='stepfilled',
+        label='not corrected\n(bias = {})'.format(ms(noncorr - true_par[i])),
+        color='lightgray', zorder=0, density=True
+    )
+    ax.hist(
+        altcorr, bins='auto', histtype='step',
+        label='reference correction\n(bias = {})'.format(ms(altcorr - true_par[i])),
+        color='black', linestyle='--', zorder=1, density=True
     )
     ax.hist(
         corr, bins='auto', histtype='step',
-        label='corrected\nbias = {}'.format(ms(corr - true_par[i]))
+        label='our correction\n(bias = {})'.format(ms(corr - true_par[i])),
+        color='black', zorder=2, density=True
     )
-    ax.plot(2 * [true_par[i]], ax.get_ylim(), scaley=False, label='true')
+    ax.plot(
+        2 * [true_par[i]], ax.get_ylim(),
+        scaley=False, label='true value', color='darkgray', linewidth=3,
+        zorder=0.5
+    )
     ax.legend(loc='best', fontsize='small')
+    ax.set_title(f'Parameter {i}')
+    ax.set_xlabel(f'parameter {i}')
+    ax.set_ylabel('normalized counts')
 
 for i in range(N):
     for j in range(i + 1, N):
         ax = axs[i][j]
         
-        ok = np.abs(biases[i]) < SIGMA_FACTOR * sigmas[i]
-        ok &= np.abs(biases[j]) < SIGMA_FACTOR * sigmas[j]
-        
-        ax.plot((estimates[i] - biases[i])[ok], (estimates[j] - biases[j])[ok], 'x', label='corrected')
-        ax.plot((estimates[i] - biases[i])[~ok], (estimates[j] - biases[j])[~ok], 'x', color='red')
-        ax.plot(estimates[i][ok], estimates[j][ok], '.', markersize=2, label='not corrected')
-        ax.plot(estimates[i][~ok], estimates[j][~ok], '.', markersize=2, color='red')
+        ax.plot(
+            true_par[j], true_par[i],
+            marker='+', linestyle='', color='black', markersize=8,
+            zorder=5, label='true value'
+        )
+        ax.plot(
+            estimates[j], estimates[i],
+            marker='.', markersize=6, color='lightgray',
+            linestyle='', label='not corrected'
+        )
+        ax.plot(
+            (estimates[j] - biases[j])[ok], (estimates[i] - biases[i])[ok],
+            marker='.', markersize=2, color='black',
+            linestyle='', label='our correction'
+        )
+        ax.plot(
+            (estimates[j] - biases[j])[~ok], (estimates[i] - biases[i])[~ok],
+            marker='x', markersize=6, color='black',
+            linestyle='', label='correction detected too large'
+        )
         ax.legend(loc='best', fontsize='small')
+        ax.set_title(f'Parameters $(0, 1)$')
+        ax.set_xlabel(f'parameter {j}')
+        ax.set_ylabel(f'parameter {i}')
+    
+for i in range(N):
+    for j in range(i):
+        ax = axs[i][j]
+        
+        k = i * N + j
+        ax.errorbar(
+            info['true_x'], info['true_y'],
+            xerr=1, yerr=1,
+            linestyle='', marker='', color='gray',
+            label='true points'
+        )
+        ax.plot(
+            table[k]['data_x'], table[k]['data_y'],
+            linestyle='', marker='.', color='black', label='simulated data'
+        )
+        ax.legend(loc='best', fontsize='small')
+        ax.set_title('Example dataset')
+        ax.set_xlabel('t')
+        ax.set_ylabel('x')
 
+fig.tight_layout()
 fig.show()
--- dev/fit.py
+++ dev/fit.py
@@ -8,10 +8,10 @@ M = 100 # number of monte carlo
 N = 2 # number of parameters
 def mu(x, p):
     return p[0] * np.cos(x / p[1])
-true_x = np.linspace(0, 20, 10)
-true_par = np.array([10, 3])
+true_x = np.linspace(0, 30, 10)
+true_par = np.array([10, 4])
 
-##########################################################
+##################################
 
 table = nplf.open_memmap('fit.npy', mode='w+', shape=(M,), dtype=[
     ('success', bool),
@@ -22,7 +22,9 @@ table = nplf.open_memmap('fit.npy', mode='w+', shape=(M,), dtype=[
     ('data_x', float, len(true_x)),
     ('complete_estimate', float, N + len(true_x)),
     ('complete_bias', float, N + len(true_x)),
-    ('complete_cov', float, (N + len(true_x), N + len(true_x)))
+    ('complete_cov', float, (N + len(true_x), N + len(true_x))),
+    ('alt_bias', float, N),
+    ('has_alt_bias', bool)
 ])
 
 def res(p, data):
@@ -44,11 +46,13 @@ dfdp = autograd.jacobian(f, 0)
 dfdpdy = autograd.jacobian(dfdp, 1)
 dfdpdp = autograd.jacobian(dfdp, 0)
 
+true_y = mu(true_x, true_par)
+
 for i in progressbar.progressbar(range(M)):
     data_x = true_x + np.random.randn(len(true_x))
-    data_y = mu(true_x, true_par) + np.random.randn(len(true_x))
+    data_y = true_y + np.random.randn(len(true_x))
     data = np.concatenate([data_x, data_y])
-
+    
     p0 = np.concatenate([true_par, true_x])
     result = optimize.least_squares(res, p0, jac=jac, args=(data,))
     table[i]['success'] = result.success
@@ -75,11 +79,28 @@ for i in progressbar.progressbar(range(M)):
     assert(B.shape == (N + len(true_x), 2 * len(true_x), 2 * len(true_x)))
     B_ = B.reshape(N + len(true_x), 4 * len(true_x) * len(true_x))
 
-    hess = np.linalg.solve(dfdp_, B_).reshape(N + len(true_x), 2 * len(true_x), 2 * len(true_x))
+    hess = np.linalg.solve(dfdp_, B_)
+    hess = hess.reshape(N + len(true_x), 2 * len(true_x), 2 * len(true_x))
     assert(np.allclose(hess, np.einsum('aji', hess)))
 
     bias = 1/2 * np.einsum('aii', hess)
     
+    data1 = np.concatenate([data_x[::2], data_y[::2]])
+    p01 = np.concatenate([true_par, true_x[::2]])
+    result1 = optimize.least_squares(res, p01, jac=jac, args=(data1,))
+    
+    data2 = np.concatenate([data_x[1::2], data_y[1::2]])
+    p02 = np.concatenate([true_par, true_x[1::2]])
+    result2 = optimize.least_squares(res, p02, jac=jac, args=(data2,))
+    
+    if result1.success and result2.success:
+        table[i]['alt_bias'] = 0.5 * (result1.x[:N] + result2.x[:N]) - result.x[:N]
+        table[i]['has_alt_bias'] = True
+    else:
+        table[i]['alt_bias'] = 0
+        table[i]['has_alt_bias'] = False
+        print(f'alt bias fit failed for i = {i}')
+
     table[i]['estimate'] = result.x[:N]
     table[i]['bias'] = bias[:N]
     table[i]['cov'] = cov[:N, :N]
@@ -88,7 +109,13 @@ for i in progressbar.progressbar(range(M)):
     table[i]['complete_estimate'] = result.x
     table[i]['complete_bias'] = bias
     table[i]['complete_cov'] = cov
+    
 
 table.flush()
 
-np.savez('fit-info.npz', true_x=true_x, true_par=true_par)
+np.savez(
+    'fit-info.npz',
+    true_x=true_x,
+    true_par=true_par,
+    true_y=true_y
+)
